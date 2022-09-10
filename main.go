ce8cb425564bc126c2f6774683d09a514bf4a5e4
Dinesh Thakur <t.diku63@gmail.com>
2022-09-10 13:50:24 +0530

    Added config variables codes

--- main.go
+++ main.go
@@ -6,12 +6,15 @@ import (
 	"SecretSanta/repository"
 	"SecretSanta/service"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
 )
 
 var (
-	personRepository repository.PersonRepository = repository.NewFlatFileRepository()
+	// personRepository repository.PersonRepository = repository.NewFlatFileRepository()
+	personRepository repository.PersonRepository = repository.NewCouchbaseRepository()
 
 	personServices service.PersonServices = service.NewPersonService(personRepository)
 
@@ -22,7 +25,17 @@ var (
 
 func main() {
 
-	const port string = ":8382"
+	//Loading the Configuration Files
+	err := godotenv.Load("configurations/config.env")
+
+	if err != nil {
+		logrus.Errorln("Error while loading the Configuration Variables: ", err)
+	}
+
+	//The Application Port Number
+	//const port string = ":8382"
+	port := os.Getenv("APPLICATION_PORT")
+	logrus.Infoln(port)
 
 	httpRouter.GET("/", func(response http.ResponseWriter, request *http.Request) {
 		logrus.Info("Server Is Running !!")
