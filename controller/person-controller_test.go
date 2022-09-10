7a9d14f81b6fbd19d9a73c02c6b1a1a8d10034ac
Dinesh Thakur <t.diku63@gmail.com>
2022-09-10 13:51:09 +0530

    added create personwish test

--- controller/person-controller_test.go
+++ controller/person-controller_test.go
@@ -7,13 +7,10 @@ import (
 	"bytes"
 	"encoding/json"
 	"io"
-	"io/ioutil"
-	"log"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/golang/mock/gomock"
-	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -26,25 +23,6 @@ func TestAddPersonWishFile(t *testing.T) {
 
 	//mockService Creating mockrepository varible
 	mockService := mocks.NewMockPersonServices(control)
-	//mockRepo := mocks.NewMockPersonRepository(control)
-
-	//Read from the File.
-	readFile, err := ioutil.ReadFile("db.json")
-	if err != nil {
-		log.Print("error while reading the data", err)
-	}
-
-	//Define Slice of PersonWishes
-	var testentitypersons entity.PersonWish
-
-	//Unmarshall it
-	if err = json.Unmarshal([]byte(readFile), &testentitypersons); err != nil {
-		log.Print("erroer unmarshallin the data", err)
-	}
-
-	//Creating the expectation and call the required function
-	mockService.EXPECT().CreatePersonWish(gomock.Any()).Return(gomock.Any(), nil)
-	logrus.Info(testentitypersons)
 
 	//Mocking the services Constructor with mockRepository for testing
 	testService := controller.NewPersonController(mockService)
@@ -64,61 +42,17 @@ func TestAddPersonWishFile(t *testing.T) {
 	//Check Status code of the reponse
 	statusCode := response.Code
 
-	logrus.Info("The response status code is :", statusCode)
-
 	//Decode the http responce
 	var person entity.PersonWish
 	json.NewDecoder(io.Reader(response.Body)).Decode(&person)
 
 	//assertions for failing
-	//	assert.Equal(t, 200, statusCode)
+	//assert.Equal(t, 200, statusCode)
 	assert.Equal(t, 500, statusCode)
-	assert.Equal(t, "Dayna", testentitypersons.Name)
+	assert.Equal(t, "", person.Name)
+	assert.Equal(t, []string(nil), person.Wishes)
 
 }
-
-// func TestAddPersonWish(t *testing.T) {
-// 	//Control Initilizing the Mock Controller
-// 	control := gomock.NewController(t)
-
-// 	//Removing the instance of controller
-// 	defer control.Finish()
-
-// 	//mockService Creating mockrepository varible
-// 	mockService := mocks.NewMockPersonServices(control)
-
-// 	//Data to be mocked
-// 	personWishes := entity.PersonWish{Name: "John", Wishes: []string{"BatMobile", "Destiny"}}
-
-// 	//Creating the expectation and call the required function
-// 	mockService.EXPECT().CreatePersonWish(gomock.Any()).Return(&personWishes, nil)
-
-// 	//Mocking the services Constructor with mockRepository for testing
-// 	testService := controller.NewPersonController(mockService)
-
-// 	//response to the test func
-// 	response := httptest.NewRecorder()
-
-// 	//PersonWish byte array to request body
-// 	personWishesByte := []byte(`{Name: "John", Wishes: []string{"BatMobile", "Destiny"}`)
-
-// 	//request for new request to test func
-// 	request := httptest.NewRequest("POST", "/addPersonWish", bytes.NewBuffer(personWishesByte))
-
-// 	//pass reuqest and responce to cotroller fun
-// 	testService.AddPersonWish(response, request)
-
-// 	//Check Status code of the reponse
-// 	statusCode := response.Code
-
-// 	//Decode the http responce
-// 	var person entity.PersonWish
-// 	json.NewDecoder(io.Reader(response.Body)).Decode(&person)
-
-// 	//assertions for failing
-// 	assert.Equal(t, 500, statusCode)
-
-// }
 
 func TestGetPersonWish(t *testing.T) {
 
